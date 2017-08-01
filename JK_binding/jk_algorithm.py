d1166d11d6406853544b695db6c3373d4a147451
Boyi Zhang <bz11252@uga.edu>
2017-08-01 15:47:59 -0400

    updated jk builder

--- JK_binding/jk_algorithm.py
+++ JK_binding/jk_algorithm.py
@@ -16,7 +16,7 @@ H 1 1.1 2 104
 """)
 
 # Build a ERI tensor
-basis = psi4.core.BasisSet.build(mol, target="cc-pvdz")
+basis = psi4.core.BasisSet.build(mol, target="cc-pvtz")
 mints = psi4.core.MintsHelper(basis)
 I = np.array(mints.ao_eri())
 
@@ -29,21 +29,26 @@ D = (D + D.T) / 2
 t1 = time.time()
 J_ref = np.einsum("pqrs,rs->pq", I, D)
 t2 = time.time()
-print("J build time:", t2-t1)
+print("J_ref build time:", t2-t1)
+
+
+t1 = time.time()
 K_ref = np.einsum("prqs,rs->pq", I, D)
+t2 = time.time()
+print("K_ref build time:", t2-t1)
 I_ = np.zeros_like(D)
-def index(i, j):
-    if i < j:
-        return j*(j + 1)/2
-    else:
-        return i*(i+1)/2
-for i in range(nbf):
-    for j in range(nbf):
-        ij = index(i,j)
-        for k in range(nbf):    
-            for l in range(nbf):
-                kl = index(k,l)
-                I_[ij,kl] = I[i,j,k,l]
+#def index(i, j):
+#    if i < j:
+#        return j*(j + 1)/2
+#    else:
+#        return i*(i+1)/2
+#for i in range(nbf):
+#    for j in range(nbf):
+#        ij = index(i,j)
+#        for k in range(nbf):    
+#            for l in range(nbf):
+#                kl = index(k,l)
+#                I_[ij,kl] = I[i,j,k,l]
     # Your implementation
 t1 = time.time()
 J = jk_binding.form_j(I.reshape(nbf*nbf, nbf*nbf),D.reshape(nbf*nbf))
@@ -51,11 +56,18 @@ J = J.reshape(nbf, nbf)
 t2 = time.time()
 print("J build time:", t2-t1)
 
+t1 = time.time()
+J_loop = jk_binding.form_j_loop(I,D)
+t2 = time.time()
+print("J build loop time:", t2-t1)
 #K = np.random.rand(nbf, nbf)
 t1 = time.time()
 K = jk_binding.form_k(I,D)
 t2 = time.time()
 print("K build time:", t2-t1)
+#print(J_loop)
+#print(J_ref)
 # Make sure your implementation is correct
 print("J is correct: %s" % np.allclose(J, J_ref))
+print("J loop is correct: %s" % np.allclose(J_loop, J_ref))
 print("K is correct: %s" % np.allclose(K, K_ref))
--- JK_binding/jk_binding.cpp
+++ JK_binding/jk_binding.cpp
@@ -5,34 +5,27 @@
 #include<string>
 #include<lawrap/blas.h>
 #include<cmath>
+#include<omp.h>
 
 namespace py = pybind11; //this is what we are using to bind to python
 
-
 //using lapack to do operations 
 //takes a numpy array and returns a numpy array
-std::vector<py::array_t<double>> form_jk(py::array_t<double> I_J,
-                            py::array_t<double> D_J,
-                            py::array_t<double> I_K,
-                            py::array_t<double> D_K)
+py::array_t<double> form_j(py::array_t<double> I,
+                            py::array_t<double> D)
 {
-    std::vector<py::array_t<double>> list_arr;
-    py::buffer_info I_J_info = I_J.request(); //function you call to get buffer info
-    py::buffer_info D_J_info = D_J.request(); //function you call to get buffer info
-    py::buffer_info I_K_info = I_K.request(); //function you call to get buffer info
-    py::buffer_info D_K_info = D_K.request(); //function you call to get buffer info
+    py::buffer_info I_info = I.request(); //function you call to get buffer info
+    py::buffer_info D_info = D.request(); //function you call to get buffer info
     
-    size_t n_j = D_J_info.shape[0];
+    size_t n = D_info.shape[0];
     
-    const double * I_J_data = static_cast<double *>(I_J_info.ptr); //cast void * to double * 
-    const double * D_J_data = static_cast<double *>(D_J_info.ptr); //cast void * to double * 
-    const double * I_K_data = static_cast<double *>(I_K_info.ptr); //cast void * to double * 
-    const double * D_K_data = static_cast<double *>(D_K_info.ptr); //cast void * to double * 
+    const double * I_data = static_cast<double *>(I_info.ptr); //cast void * to double * 
+    const double * D_data = static_cast<double *>(D_info.ptr); //cast void * to double * 
     
-    std::vector<double> J_data(n_j); //std::vector calls new but will automatically delete when out of scope
+    std::vector<double> J_data(n); //std::vector calls new but will automatically delete when out of scope
     std::cout << "before gemv" << '\n';
 
-    LAWrap::gemv('N', n_j, n_j, 1, I_J_data, n_j, D_J_data, 1, 0, J_data.data(), 1);
+    LAWrap::gemv('N', n, n, 1, I_data, n, D_data, 1, 0, J_data.data(), 1);
     std::cout << "after gemv" << '\n';
 
     py::buffer_info Jbuf = 
@@ -42,26 +35,92 @@ std::vector<py::array_t<double>> form_jk(py::array_t<double> I_J,
         sizeof(double), //size of double
         py::format_descriptor<double>::format(),//format descriptor
         1, //number of dimensions 
-        {n_j},//automatically construct a vector with whatever in curly braces 
+        {n},//automatically construct a vector with whatever in curly braces 
         {1 * sizeof(double)} //leading dimension
     };
     //std vector initialization with {}
     //std::vector<double> s = {1.0,2.0,..}
 
-    size_t n = D_K_info.shape[0];
+    return py::array_t<double>(Jbuf);
+}
+py::array_t<double> form_j_loop(py::array_t<double> I,
+                            py::array_t<double> D)
+{
+    py::buffer_info I_info = I.request(); //function you call to get buffer info
+    py::buffer_info D_info = D.request(); //function you call to get buffer info
+    
+    size_t n = D_info.shape[0];
+    
+    const double * I_data = static_cast<double *>(I_info.ptr); //cast void * to double * 
+    const double * D_data = static_cast<double *>(D_info.ptr); //cast void * to double * 
+    
+    std::vector<double> J_data(n*n); //std::vector calls new but will automatically delete when out of scope
+
+#pragma omp parallel
+{
+    std::cout << "number of threads using:" << omp_get_max_threads() << "\n";
+#pragma omp for 
+        for(int p = 0; p < n; p++)
+        {
+            for(int q = 0; q <= p; q++)
+            {
+                for(int r = 0; r < n; r++)
+                {
+                    for(int s = 0; s <= r; s++)
+                    {
+                        int test = p*pow(n,3)+q*pow(n,2)+r*n + s;
+                        if (r==s)
+                            J_data[p*n + q] += I_data[test] * D_data[r*n + s];
+                        else 
+                            J_data[p*n + q] += 2*(I_data[test] * D_data[r*n + s]);
+
+                        J_data[q*n +p] = J_data[p*n +q];
+                    }
+                }
+            }
+        }
+}
+    py::buffer_info Jbuf = 
+    {
+        J_data.data(), //pointer 
+        //pybind11 takes pointer and shape, copies data, allocates new memory - so even though pointer go out of scope okay
+        sizeof(double), //size of double
+        py::format_descriptor<double>::format(),//format descriptor
+        2, //number of dimensions 
+        {n,n},//automatically construct a vector with whatever in curly braces 
+        {n * sizeof(double), sizeof(double)} //leading dimension
+    };
+    //std vector initialization with {}
+    //std::vector<double> s = {1.0,2.0,..}
+
+    return py::array_t<double>(Jbuf);
+}
+
+py::array_t<double> form_k(py::array_t<double> I,
+                            py::array_t<double> D)
+{
+    py::buffer_info I_info = I.request(); //function you call to get buffer info
+    py::buffer_info D_info = D.request(); //function you call to get buffer info
+    
+    size_t n = D_info.shape[0];
+    
+    const double * I_data = static_cast<double *>(I_info.ptr); //cast void * to double * 
+    const double * D_data = static_cast<double *>(D_info.ptr); //cast void * to double * 
     
     std::vector<double> K_data(n*n); //std::vector calls new but will automatically delete when out of scope
 
+#pragma omp parallel for
     for(int p = 0; p < n; p++)
     {
         for(int r = 0; r < n; r++)
         {
-            for(int q = 0; q < n; q++)
+            for(int q = 0; q <= p; q++)
             {
                 for(int s = 0; s < n; s++)
                 {
                     int test = p*pow(n,3)+r*pow(n,2)+q*n + s;
-                    K_data[p*n + q] += I_K_data[test] * D_K_data[r*n + s]; 
+                    K_data[p*n + q] += I_data[test] * D_data[r*n + s];
+                    K_data[q*n +p] = K_data[p*n +q];
                 }
             }
         }
@@ -80,16 +139,16 @@ std::vector<py::array_t<double>> form_jk(py::array_t<double> I_J,
     //std vector initialization with {}
     //std::vector<double> s = {1.0,2.0,..}
 
-    list_arr.push_back(Jbuf);       
-    list_arr.push_back(Kbuf);       
-    return list_arr;
+    return py::array_t<double>(Kbuf);
 }
 PYBIND11_PLUGIN(jk_binding) //need this function to use (macros?) 
 //wrap c binding for you 
 {
     py::module m("jk_binding", "A basic module");
     
-    m.def("form_jk", &form_jk);
+    m.def("form_j", &form_j);
+    m.def("form_k", &form_k);
+    m.def("form_j_loop", &form_j_loop);
     //converts python list to std vector and vice versa
     //need to include pybind11 stl.h 
     return m.ptr();
