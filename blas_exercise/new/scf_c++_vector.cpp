a85d2449daa643279365224cc54143a980db2efd
Ashutosh Kumar <akumar1@vt.edu>
2017-08-02 20:27:46 -0400

    proper use of std::shared_ptr<vector<double>>

--- blas_exercise/new/scf_c++_vector.cpp
+++ blas_exercise/new/scf_c++_vector.cpp
@@ -4,8 +4,9 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <memory>
 
-std::vector<double> operator+(std::vector<double>& left_vec, std::vector<double>& right_vec)
+/*std::vector<double> operator+(std::vector<double>& left_vec, std::vector<double>& right_vec)
 { 
     if (left_vec.size() > right_vec.size())
     {
@@ -32,15 +33,25 @@ std::vector<double> operator+(std::vector<double>& left_vec, std::vector<double>
   
 
 
-void read_function(std::vector<double> & vec, std::string filename, int nbas) // pass the vector by reference to avoid embarassment
-{																			  // its not advisable to use shared_ptr<std::vector<double>>	
-    std::ifstream input(filename);											  // as it adds an extra layer of pointers to the vector object  
-    double number;													          // which already uses shared_ptrs under the hood. So, shared_ptrs	
-    for (int i = 0; i < nbas * nbas; i++){									  // are very useful for custom classes. Fot standard container objects 	
-            input >> number;											      // unique_ptrs should be preferred over shared_ptrs.
+void read_function(std::vector<double> & vec, std::string filename, int nbas) 
+{																			 
+    std::ifstream input(filename);											
+    double number;													       
+    for (int i = 0; i < nbas * nbas; i++){								
+            input >> number;											      
             vec.push_back(number); // row major format
         }
 }
+
+// pass the vector by reference to avoid embarassment
+// its not advisable to use shared_ptr<std::vector<double>>   
+// as it adds an extra layer of pointers to the vector object  
+// which already uses shared_ptrs under the hood. So, shared_ptrs 
+// are very useful for custom classes. Fot standard container objects     
+// unique_ptrs should be preferred over shared_ptrs.
+
+
+
 int main()
 {
     int nbas = 7;
@@ -82,3 +93,59 @@ int main()
 
     return 0;
 }
+*/
+
+void read_function(std::shared_ptr<std::vector<double>>  vec, std::string filename, int nbas)
+{
+    std::ifstream input(filename);
+    double number; 
+    for (int i = 0; i < nbas * nbas; i++){
+            input >> number;
+            vec->push_back(number); // row major format
+        }
+}
+
+
+
+int main()
+{
+    int nbas = 7;
+    int nocc = 5;
+	auto H = std::make_shared<std::vector<double>>();
+	auto F = std::make_shared<std::vector<double>>();
+	auto C = std::make_shared<std::vector<double>>();
+	auto S = std::make_shared<std::vector<double>>();
+
+    read_function(H, "H.data", nbas);
+    read_function(F, "F.data", nbas);
+    read_function(C, "C.data", nbas);
+    read_function(S, "S.data", nbas);
+
+	auto D = std::make_shared<std::vector<double>>(nbas * nbas);
+	auto DS = std::make_shared<std::vector<double>>(nbas * nbas);
+	auto F_plus_H = std::make_shared<std::vector<double>>(nbas * nbas);
+
+    LAWrap::gemm('T', 'N', nbas, nbas, nocc, 2, C->data(), nbas, C->data(), nbas, 0, D->data(), nbas); // everything in column major format: D = 2.0 * C.T * C
+
+    LAWrap::gemm('N', 'T', nbas, nbas, nbas, 1, D->data(), nbas, S->data(), nbas, 0, DS->data(), nbas);
+
+    double nelec = 0;
+    for (int i = 0; i < nbas; i++)
+    {
+        nelec += (* DS)[i*nbas + i];
+    }
+    std::cout << nelec << '\n';
+
+    F_plus_H = H;   // '='  operator overloading
+    std::transform(H->begin(), H->end(), F->begin(), 
+              F_plus_H->begin(), std::plus<double>());
+
+
+    double scf_energy = LAWrap::dot(nbas*nbas, F_plus_H->data(), 1, D->data(), 1);
+    scf_energy /= 2;
+
+    std::cout << scf_energy << '\n';
+
+    return 0;
+}
+
