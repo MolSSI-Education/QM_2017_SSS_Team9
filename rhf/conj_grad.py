23323a7906b641ff586cf9a7a88f694608e07fc3
Ashutosh Kumar <akumar1@vt.edu>
2017-08-02 20:16:43 -0400

    cg direct works now as a standalone func

--- rhf/conj_grad.py
+++ rhf/conj_grad.py
@@ -1,34 +1,66 @@
 import numpy as np
-import time
+"""
+Contains the conjugate gradient helper function
+"""
+def helper_PCG_direct(A, b, tol=1e-10, max_iter=None, x0=None, M=None):
+    """
+    Solves a linear system of equations Ax = b using a preconditioned conjugate 
+    gradient method within a user-defined tolerance
+    
 
+    Parameters
+    ----------
+    A : LHS Matrix (2-dimensional numpy array)
+    b : RHS vector (1-dimensional numpy array)
+    tol : User-defined tolerance for residual convergence (Default = 1.e-10)
+    max_iter : User-defined maximum number of iterations for convergence (Default = 2 * len(b))
+    x0 : User-specified guess vector (Default = zero-array)
+    M : Preconditioner Matrix (2-dimensional numpy array)  (Default = Identity Matrix)  
 
-def conj_grad(A, b):
+    Returns
+    -------
+    x : solution vector of Ax = b
+
+    Notes
+    -----
+    
+    Examples
+    --------
+
+    x = helper_PCG_direct(A, b)
+    ...
+
+    """
+
+    if max_iter is None:
+        max_iter = 2*len(b)
+    if x0 is None:
+        x = np.zeros_like(b)
+    else:
+        x = x0
+    if M is None:
+        M = np.diag(A)
+
+    r = b - np.dot(A,x)
+    z = r/M
     n = len(b)
-    x = np.ones(n) 
-    r = np.dot(A, x) - b
-    p = - r
-    res_k_norm = np.dot(r, r)
-    for i in range(2*n):
+    p = z
+    res_k_norm = np.dot(r, z)
+    print("Starting Conjugate Gradient Iterations...\n")
+    for iteration in range(2*n):
         Ap = np.dot(A, p)
         alpha = res_k_norm / np.dot(p, Ap)
         x += alpha * p
-        r += alpha * Ap
-        res_kplus1_norm = np.dot(r, r)
+        r -= alpha * Ap
+        z = r/M
+        res_kplus1_norm = np.dot(r, z)
         beta = res_kplus1_norm / res_k_norm
         res_k_norm = res_kplus1_norm
-        if res_kplus1_norm < 1e-14:
-            print('Total iterations:', i)
+        rms = np.sqrt(np.sum(r**2) / len(r))
+        print('CG Iteration %3d: RMS = %3.8f' % (iteration, rms))
+        if (res_kplus1_norm < tol) or (iteration > max_iter):
+            print('\nConverged!!\n')
             break
-        p = beta * p - r
+        p = beta * p + z
     return x
-
-if __name__ == '__main__':
-    n = 100
-    A = np.random.normal(size=[n, n])
-    A = np.dot(A.T, A)
-    b = np.ones(n)
-    xo = np.zeros(n)
-    x = conj_grad(A, b, xo)
-    x1 = np.linalg.solve(A, b)
-    print(" Solution matches with numpy's cg solver: %s" % np.allclose(x, x1, 1e-9))
 
